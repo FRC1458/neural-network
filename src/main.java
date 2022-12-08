abefc9160626e06e53f2f7c7c0e90bbe8795b716
YahyaHasan <97994926+YahyaHasan@users.noreply.github.com>
2022-12-07 16:49:23 -0800

    Beluga Whales!

--- src/main.java
+++ src/main.java
@@ -7,7 +7,7 @@ public static void main(String args[]){
     System.out.println("Hello World");
     
     for (int i = 0; i < BELUGA_WHALE_SWIM_RATE % 10; i++) {
-    System.out.println("Beluga Whale #" + i + 1);
+    System.out.println("I love Beluga WHales! #" + i + 1);
     }
 }
 }
