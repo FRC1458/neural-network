751554b77bb6d7fdf385c2e96a8dc6981fd5ba69
YahyaHasan <97994926+YahyaHasan@users.noreply.github.com>
2022-12-07 16:43:28 -0800

    Better update

--- src/main.java
+++ src/main.java
@@ -1,7 +1,13 @@
 import javax.print.event.PrintJobListener;
 
+const int BELUGA_WHALE_SWIM_RATE = 17; //Beluga whale swims @ 17 mph!!!!!!
+
 public class main {
 public static void main(String args[]){
     System.out.println("Hello World");
-}    
+    
+    for (int i = 0; i < BELUGA_WHALE_SWIM_RATE % 10; i++) {
+    System.out.println("Beluga Whale #" + i + 1);
+    }
+}
 }
